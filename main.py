bdcb0c210c5c3857bc63747ed20f88d907465375
Matthew Martin <zhintateo19924559@gmail.com>
2019-07-08 16:09:01 -0700

    Reading the first five processes

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from tkinter import *
+#! /usr/bin/env python
 
-class InventoryManager(object):
-	"""docstring for InventoryManager"""
-	def __init__(self, master):
-		self.master = master
-		master.title = ("Invenger")
+import wmi
 
+c = wmi.WMI()
+countP = 0
+wql = "SELECT IPAddress FROM Win32_NetworkAdapterConfiguration WHERE IPEnabled = 'True'"
+process_watcher = c.Win32_Process.watch_for("operation")
 
-root = Tk()
-my_gui = InventoryManager(root)
-root.mainloop()
+print ('OS is: {0}'.format(c.Win32_OperatingSystem() [0].Caption))
+print ('Disk freespace [0] - total {1}'.format(c.Win32_LogicalDisk() [0].Freespace,c.Win32_LogicalDisk()[0].Size))
+print ('Total Memory: {0}'.format(c.Win32_ComputerSystem() [0].TotalPhysicalMemory))
+
+print('Local IP address: {0}'. format(''.join(c.query(wql)[0].IPAddress)))
+
+while countP < 5:
+	countP += 1
+	current_process = process_watcher()
+	print(current_process.Caption)
